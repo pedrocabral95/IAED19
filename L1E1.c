4c3593044c82ca174afd888269491c676eeda556
pedrocabral95 <47759214+pedrocabral95@users.noreply.github.com>
2019-03-12 20:57:36 +0000

    Create L1E1.c

---
+++ L1E1.c
@@ -0,0 +1,10 @@
+/*Crie um ficheiro ex01.c com um programa em C que imprima hello, world\n
+Input: None
+Output: Hello, World
+*/
+#include <stdio.h>
+
+int main() {
+	printf("Hello, World\n");
+	return 0;
+}
