9a2660c94dc2eb09e804aebf69e92247a5197733
pedrocabral95 <47759214+pedrocabral95@users.noreply.github.com>
2019-03-12 21:02:00 +0000

    Create L1E2.c

---
+++ L1E2.c
@@ -0,0 +1,13 @@
+/*
+Crie um ficheiro ex02.c com um programa em C que imprima Ola, mundo!\n.
+
+Input: None
+Output: Olá, mundo!
+*/
+
+#include <stdio.h>
+
+int main() {
+  printf("Olá, mundo\n");
+  return 0;
+ }
